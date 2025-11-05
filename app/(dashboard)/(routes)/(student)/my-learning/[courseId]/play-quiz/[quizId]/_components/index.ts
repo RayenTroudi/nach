b1329b6ae36397a1bfb584deadf3534de746000d
Rayen troudi <rayentroudi00@gmail.com>
2025-11-05 16:45:53 +0100

    Remove quiz functionality - deleted play-quiz directory with all quiz-related components

--- app/(dashboard)/(routes)/(student)/my-learning/[courseId]/play-quiz/[quizId]/_components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as InitialScreen } from "./InitialScreen";
-export { default as Questions } from "./Questions";
-export { default as Question } from "./Question";
-export { default as ResultScreen } from "./ResultScreen";

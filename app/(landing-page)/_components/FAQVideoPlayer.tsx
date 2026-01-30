04ac3ed51734b03e3eb4b3bf73a9c68d0b06e563
Rayen troudi <rayentroudi00@gmail.com>
2026-01-30 11:36:42 +0100

    some

--- app/(landing-page)/_components/FAQVideoPlayer.tsx
+++ app/(landing-page)/_components/FAQVideoPlayer.tsx
@@ -168,8 +168,7 @@ export default function FAQVideoPlayer({
                   }
                 }}
               />
-            )}}}
-            />
+            )}
           </motion.div>
         </AnimatePresence>
 

fa7d36c449e7fa5636867c90eb42721788b455ae
Rayen Troudi <94689247+RayenTroudi@users.noreply.github.com>
2026-02-20 17:43:16 +0000

    fixed the build

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,38 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/api/review",
-  "/api/uploadthing",
-  "/api/webhooks(.*)",
-  "/api/courses",
-  "/api/ausbildung",
-  "/api/video-proxy",
-  "/api/video-stream",
-  "/api/auth/reset-password",
-  "/api/auth/password-changed",
-  "/api/health",
-  "/courses/(.*)",
-  "/course/(.*)",
-  "/user/(.*)",
-  "/about",
-  "/blog",
-  "/blog/(.*)",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/forgot-password",
-  "/reset-password",
-  "/contact(.*)",
-  "/documents(.*)",
-  "/(en|de|ar)",
-  "/(en|de|ar)/sign-in(.*)",
-  "/(en|de|ar)/sign-up(.*)",
-  "/(en|de|ar)/forgot-password",
-  "/(en|de|ar)/reset-password",
-]);
-
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/api/review",
+    "/api/uploadthing",
+    "/api/webhooks(.*)",
+    "/api/courses",
+    "/api/ausbildung",
+    "/api/video-proxy",
+    "/api/video-stream",
+    "/api/auth/reset-password",
+    "/api/auth/password-changed",
+    "/api/health",
+    "/courses/(.*)",
+    "/course/(.*)",
+    "/user/(.*)",
+    "/about",
+    "/blog",
+    "/blog/(.*)",
+    "/sign-in(.*)",
+    "/sign-up(.*)",
+    "/forgot-password",
+    "/reset-password",
+    "/contact(.*)",
+    "/documents(.*)",
+    "/(en|de|ar)",
+    "/(en|de|ar)/sign-in(.*)",
+    "/(en|de|ar)/sign-up(.*)",
+    "/(en|de|ar)/forgot-password",
+    "/(en|de|ar)/reset-password",
+  ],
 });
 
 export const config = {
-  matcher: ["/((?!_next|.*\\..*|public).*)", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
